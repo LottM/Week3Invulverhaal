965f57a3d1657600827e09e3583b94f445479ce7
LottM <lottevandermolen@gmail.com>
2015-11-09 16:13:24 +0100

    Initial Commit

---
+++ Week3MadLibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week3MadLibs
+//
+//  Created by Lotte van der Molen on 09/11/15.
+//  Copyright © 2015 Lotte van der Molen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
