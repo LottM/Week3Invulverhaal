0144fa46d18a03ed959f233d3fcbfc47d8692857
LottM <lottevandermolen@gmail.com>
2015-11-11 19:12:11 +0100

    Layout of the homepage fixed (using stacks and constraints), and adding segue and unwind segue

---
+++ Week3MadLibs/View2.swift
@@ -0,0 +1,21 @@
+//
+//  View2.swift
+//  Week3MadLibs
+//
+//  Created by Lotte van der Molen on 11/11/15.
+//  Copyright © 2015 Lotte van der Molen. All rights reserved.
+//
+
+import UIKit
+
+class View2: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
