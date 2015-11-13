2bd8f230ea25937ac5e6b3fe6a45cb07d8a514c7
LottM <lottevandermolen@gmail.com>
2015-11-13 17:49:14 +0100

    Link view controllers

---
+++ Week3MadLibs/Page3ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  2ViewController.swift
+//  Week3MadLibs
+//
+//  Created by Lotte van der Molen on 13/11/15.
+//  Copyright © 2015 Lotte van der Molen. All rights reserved.
+//
+
+import UIKit
+
+class _ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
